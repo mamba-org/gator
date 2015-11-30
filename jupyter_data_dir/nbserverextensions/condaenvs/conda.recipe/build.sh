6f1226c63e2833dfa25d1e18df13b644fb768652
damianavila <damianavila@gmail.com>
2015-11-30 19:40:35 -0300

    Fix conda recipe to install the condaenvs python package.

--- jupyter_data_dir/nbserverextensions/condaenvs/conda.recipe/build.sh
+++ jupyter_data_dir/nbserverextensions/condaenvs/conda.recipe/build.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# copy the nbserver extension into site-packages
-mkdir -p $PREFIX/lib/python$PY_VER/site-packages/condaenvs
-cp -rf condaenvs/* $PREFIX/lib/python$PY_VER/site-packages/condaenvs
-
-mkdir -p $PREFIX/etc/jupyter
-cp -rf conda_env_config.py $PREFIX/etc/jupyter/conda_env_config.py
+$PYTHON setup.py install
---
+++ jupyter_data_dir/nbserverextensions/condaenvs/conda.recipe/post-link.bat
@@ -0,0 +1,1 @@
+"%PREFIX%/bin/python" -m condaenvs.install --enable --prefix="%PREFIX%"
