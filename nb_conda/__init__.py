1c7e01c311ffca8e08edca49e7611fe85c1ff0a1
damianavila <damianavila@gmail.com>
2016-02-11 19:11:42 -0300

    Update __init__ with needed function to be installed by notebook nbextension 4.2 mechanism. Also add the fucntion to use for the proposed server extension machinery.

--- nb_conda/__init__.py
+++ nb_conda/__init__.py
@@ -1,1 +1,14 @@
 from ._version import version_info, __version__
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'nbextension/static',
+        'dest': 'nb_conda',
+        'require': 'nb_conda/main'
+    }]
+
+def _jupyter_server_extension_paths():
+    return [{
+        'require': 'nb_conda.nbextension'
+    }]
