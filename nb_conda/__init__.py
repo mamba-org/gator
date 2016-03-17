f551d23531ec4aab041494ac8af921eb77d6b2a0
damianavila <damianavila@gmail.com>
2016-03-17 15:55:02 -0300

    Update to the latest way to offer metadata

--- nb_conda/__init__.py
+++ nb_conda/__init__.py
@@ -1,14 +1,12 @@
 from ._version import version_info, __version__
 
+
 def _jupyter_nbextension_paths():
-    return [{
-        'section': 'notebook',
-        'src': 'nbextension/static',
-        'dest': 'nb_conda',
-        'require': 'nb_conda/main'
-    }]
+    return [dict(section="notebook",
+                 src="nbextension/static",
+                 dest="nb_conda",
+                 require="nb_conda/main")]
+
 
 def _jupyter_server_extension_paths():
-    return [{
-        'require': 'nb_conda.nbextension'
-    }]
+    return [dict(module='nb_conda.nbextension')]
