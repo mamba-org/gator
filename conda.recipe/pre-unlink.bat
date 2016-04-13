1f42cf784be764d81f67dd7b39a37fc10bc0c46e
damianavila <damianavila@gmail.com>
2016-04-13 20:44:50 -0300

    Removing uninstall step from remaining pre-unlink

--- conda.recipe/pre-unlink.bat
+++ conda.recipe/pre-unlink.bat
@@ -1,4 +1,3 @@
 "%PREFIX%\Scripts\jupyter" nbextension disable --sys-prefix --py "%PKG_NAME%"
-"%PREFIX%\Scripts\jupyter" nbextension uninstall --sys-prefix  --py "%PKG_NAME%"
 "%PREFIX%\Scripts\jupyter" serverextension disable --sys-prefix --py "%PKG_NAME%"
 errorlevel 1 exit 1
