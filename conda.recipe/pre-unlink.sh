318b2cdf587bb295cb3310090347d691ceda6f2b
damianavila <damianavila@gmail.com>
2016-04-13 20:40:16 -0300

    Test if deleting the uninstall still clean the space

--- conda.recipe/pre-unlink.sh
+++ conda.recipe/pre-unlink.sh
@@ -1,3 +1,2 @@
 "${PREFIX}/bin/jupyter" nbextension disable --sys-prefix --py "${PKG_NAME}"
-"${PREFIX}/bin/jupyter" nbextension uninstall --sys-prefix --py "${PKG_NAME}"
 "${PREFIX}/bin/jupyter" serverextension disable --sys-prefix --py "${PKG_NAME}"
