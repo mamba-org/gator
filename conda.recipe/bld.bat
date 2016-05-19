e5cb22afec6294a5f391664ffa8ed460c64aa985
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-19 17:08:39 -0400

    setup.py args

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,1 +1,1 @@
-"%PYTHON%" setup.py install && "%PREFIX%\Scripts\jupyter-nbextension.exe" install nb_conda --py --sys-prefix --overwrite && if errorlevel 1 exit 1
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt && "%PREFIX%\Scripts\jupyter-nbextension.exe" install nb_conda --py --sys-prefix --overwrite && if errorlevel 1 exit 1
