0f5e25748875885a3f6512911b6d664a0cd5ba9f
Frederic Collonval <frederic.collonval@safrangroup.com>
2018-08-06 16:32:16 +0200

    Style: Environment toolbar

--- labextension/src/components/CondaEnvToolBar.tsx
+++ labextension/src/components/CondaEnvToolBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CondaEnvToolBarStyle } from '../styles/CondaEnvStyles';
+import { style } from 'typestyle';
 
 
 export interface CondaEnvToolBarProps {
@@ -11,11 +11,60 @@ export interface CondaEnvToolBarProps {
 
 export const CondaEnvToolBar = (props: CondaEnvToolBarProps) => {
   return (
-    <div className={CondaEnvToolBarStyle}>
-      <button onClick={props.onCreate}>Create</button>
-      <button onClick={props.onClone}>Clone</button>
-      <button onClick={props.onImport}>Import</button>
-      <button onClick={props.onRemove}>Remove</button>
+    <div className='jp-Toolbar'>
+      <button 
+        className={Style.Button} 
+        onClick={props.onCreate}>
+          Create</button>
+      <button 
+        className={Style.Button} 
+        onClick={props.onClone}>
+          Clone</button>
+      <button 
+        className={Style.Button} 
+        onClick={props.onImport}>
+          Import</button>
+      <button 
+        className={Style.Button} 
+        onClick={props.onRemove}>
+          Remove</button>
     </div>
   );
+}
+
+namespace Style {
+  export const Button = style({
+    flex: '1 1 auto',
+    display: 'inline-block',
+    height: '24px',
+    width: '32px',
+    backgroundColor: 'var(--jp-layout-color1)',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: '16px',
+    border: '1px solid var(--jp-layout-color1)',
+    paddingLeft: '8px',
+    paddingRight: '8px',
+    fontSize: 'var(--jp-ui-font-size1)',
+    lineHeight: '28px', // var(--jp-private-toolbar-height);
+
+    $nest: {
+      '&:focus': {
+        boxShadow: 'var(--jp-toolbar-box-shadow)',
+        border: '1px solid var(--jp-toolbar-border-color)'
+      },
+      '&:enabled:hover': {
+        border: '1px solid var(--jp-toolbar-border-color)',
+        boxShadow: '0px 0px 2px 0px rgba(0,0,0,0.24)'
+      },
+      '&:enabled:active': {
+        border: '1px solid var(--jp-toolbar-border-color)',
+        backgroundColor: 'var(--jp-toolbar-active-background)',
+        boxShadow: 'var(--jp-toolbar-box-shadow)'
+      },
+      '&:disabled': {
+        opacity: 0.4
+      }
+    }
+  });
 }
