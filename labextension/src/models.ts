d5a29840288393614bcb0bf878ef70c0041e8eb6
Frederic Collonval <frederic.collonval@safrangroup.com>
2018-08-10 17:24:25 +0200

    Handle the `asynchrone` request for available packages

--- labextension/src/models.ts
+++ labextension/src/models.ts
@@ -156,8 +156,27 @@ export class PackagesModel{
   constructor(environment? : string){
     this.environment = environment;
     this.packages = {};
-    this.load();
+    // this.load();
   }
+
+  private waitAnswer(response: Response): Promise<Response>{
+      return new Promise((resolve, reject) => {
+        if (response.status == 202){
+          console.log('Retry requesting the packages');
+
+          setTimeout(() => { 
+            requestServer(URLExt.join('conda', 'packages', 'available'), { method: 'GET' })
+            .then(response => {
+              resolve(this.waitAnswer(response));
+            });
+          },
+          1000)
+        } else {
+          console.log('Found the packages');
+          resolve(response);
+        }
+      });
+    }
 
   async load(): Promise<PackagesModel.IPackages>{
     if (this.environment === undefined){
@@ -166,51 +185,49 @@ export class PackagesModel{
     }
 
     try {
+      console.log('in loading package for environment ' + this.environment)
       let request: RequestInit = {
         method: 'GET'
       }
-      // // Get all available packages
-      // let all_response = await requestServer(URLExt.join('conda', 'packages', 'available'), request);
-      // let all_data = await all_response.json() as PackagesModel.IPackages;
+      // Get all available packages
+      let first_reply = await requestServer(URLExt.join('conda', 'packages', 'available'), request);
+      let available_pkgs = await this.waitAnswer(first_reply);
+      let all_data = await available_pkgs.json() as {packages: Array<PackagesModel.IPackage>};
 
       // Get installed packages
       let response = await requestServer(URLExt.join('conda', 'environments', this.environment), request);
       let data = await response.json() as {packages: Array<PackagesModel.IPackage>};
-      
-      this.packages = {};
-      data.packages.forEach(pkg => {
-        this.packages[pkg.name] = pkg;
-      });
-      return this.packages;
 
-      // // Set installed package status
-      // //- packages are sorted by name, we take advantage of this.
-      // let all_packages = all_data.packages;
-      // let final_list = new Array<PackagesModel.IPackage>();
+      // Set installed package status
+      //- packages are sorted by name, we take advantage of this.
+      let all_packages = all_data.packages;
+      let final_list = {};
 
-      // let availableIdx = 0;
-      // let installedIdx = 0;
+      let availableIdx = 0;
+      let installedIdx = 0;
 
-      // while(availableIdx < all_packages.length){
-      //   let pkg = all_packages[availableIdx];
-      //   if(installedIdx < data.packages.length){
-      //     let installed = data.packages[installedIdx];
-      //     if (pkg.name > installed.name) { // installed is not in available
-      //       availableIdx -= 1;
-      //     }
-      //     if (pkg.name === installed.name){
-      //       pkg = installed;  // If available name matching installed, installed is added as it may be updatable
-      //       pkg.installed = true;
-      //       installedIdx += 1;
-      //     }
-      //   }
-      //   final_list.push(pkg);
-      //   availableIdx += 1;
-      // }
+      while(availableIdx < all_packages.length){
+        let pkg = all_packages[availableIdx];
+        pkg.status = PackagesModel.PkgStatus.Available;
+        if(installedIdx < data.packages.length){
+          let installed = data.packages[installedIdx];
+          if (pkg.name > installed.name) { // installed is not in available
+            pkg = installed;
+            availableIdx -= 1;
+          }
+          if (pkg.name === installed.name){
+            pkg = installed;  // If available name matching installed, installed is added as it may be updatable
+            pkg.status = PackagesModel.PkgStatus.Installed;
+            installedIdx += 1;
+          }
+        }
+        final_list[pkg.name] = pkg;
+        availableIdx += 1;
+      }
 
-      // this.packages = final_list;
+      this.packages = final_list;
 
-      // return {packages: final_list};
+      return final_list;
 
     } catch (err) {
       throw new Error('An error occurred while retrieving available packages.');
@@ -313,6 +330,22 @@ export namespace PackagesModel {
     channel?: string,
     status?: PkgStatus,
     updatable?: boolean
+  }
+
+  export interface IRawPackage extends IPackage {
+    arch: string | null,
+    build_number: number,
+    channel: string,
+    constrains: Array<string>,
+    depends: Array<string>,
+    fn: string,
+    license: string,
+    md5: string,
+    platform: string | null,
+    size: number,
+    subdir: string,
+    timestamp: number,
+    url: string
   }
 
   /**
