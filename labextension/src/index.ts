e338bcd325083ae4cc204cf718eace47e47cf638
Frederic Collonval <fcollonval@gmail.com>
2018-09-09 22:39:25 +0200

    BUG Panel is not loading

--- labextension/src/index.ts
+++ labextension/src/index.ts
@@ -28,55 +28,51 @@ function activateCondaEnv(
   const { commands, shell } = app;
   const plugin_namespace = "conda-env";
   const model = new EnvironmentsModel();
-  let widget: CondaEnvWidget;
+  let widget: MainAreaWidget<CondaEnvWidget>;
 
   const command: string = "condaenv:open-ui";
 
+  commands.addCommand(command, {
+    label: "Conda Packages Manager",
+    execute: () => {
+      if (!widget) {
+        let condaWidget = new CondaEnvWidget(-1, -1, model);
+        condaWidget.addClass("jp-NbConda");
+        condaWidget.id = plugin_namespace;
+        condaWidget.title.label = "Packages";
+        condaWidget.title.caption = "Conda Packages Manager";
+        condaWidget.title.iconClass = Style.TabIcon;
+
+        widget = new MainAreaWidget({ content: condaWidget });
+      }
+      if (!tracker.has(widget)) {
+        // Track the state of the widget for later restoration
+        tracker.add(widget);
+      }
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        shell.addToMainArea(widget);
+      }
+      // Activate the widget
+      shell.activateById(widget.id);
+    }
+  });
+
+  // Add command to command palette
+  palette.addItem({ command, category: "Settings" });
+
+  // Add command to settings menu
+  menu.settingsMenu.addGroup([{ command: command }], 999);
+
+  // Track and restore the widget state
   let tracker = new InstanceTracker<MainAreaWidget<CondaEnvWidget>>({
     namespace: plugin_namespace
   });
   restorer.restore(tracker, {
     command,
-    args: widget => JSONExt.emptyObject,
-    name: widget => plugin_namespace
+    args: () => JSONExt.emptyObject,
+    name: () => plugin_namespace
   });
-
-  commands.addCommand(command, {
-    label: "Conda Packages Manager",
-    execute: () => {
-      if (tracker.currentWidget) {
-        /** Activate the widget */
-        shell.activateById(widget.id);
-        return;
-      }
-
-      if (!widget) {
-        widget = new CondaEnvWidget(-1, -1, model);
-        widget.addClass("jp-NbConda");
-      }
-
-      if (!widget.isAttached) {
-        widget.id = plugin_namespace;
-        widget.title.label = "Packages";
-        widget.title.caption = "Conda Packages Manager";
-        widget.title.iconClass = Style.TabIcon;
-
-        let main = new MainAreaWidget({ content: widget });
-        if (!tracker.has(main)) {
-          // Track the state of the widget for later restoration
-          tracker.add(main);
-        }
-        /** Attach the widget to the main work area if it's not there */
-        shell.addToMainArea(main);
-      }
-    }
-  });
-
-  /** Add command to command palette */
-  palette.addItem({ command, category: "Settings" });
-
-  /** Add command to settings menu */
-  menu.settingsMenu.addGroup([{ command: command }], 999);
 
   return model;
 }
