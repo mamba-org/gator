118daf7a5f8053c84477f68a3fdb3d57f23a8d93
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-05-11 15:01:20 +0200

    Change app logo and title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Mamba logo" src="./assets/mamba_logo.png">
+    <HelloWorld msg="Mamba Navigator"/>
   </div>
 </template>
 
