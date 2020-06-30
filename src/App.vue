84a3d5a700b30017112d68b98fdd7e63adc9f182
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-06-30 20:08:35 +0200

    Remove Vuetify dependency

--- src/App.vue
+++ src/App.vue
@@ -1,206 +1,22 @@
 <template>
-  <v-app>
-
-    <v-content>
-    <v-row>
-    <v-col
-      cols="12"
-      sm="4"
-    >
-    <v-card>
-      <v-subheader>SELECT ENVIRONMENT</v-subheader>
-      <cv-multi-select
-        :label="envSelectLabel"
-        :options="envs"
-        v-model="selectedEnv"
-      >
-      </cv-multi-select>
-    </v-card>
-    </v-col>
-
-    <v-col
-      cols="12"
-      sm="4"
-    >
-    <v-card>
-      <p>Selected environment (name): {{ selectedEnv }}</p>
-    </v-card>
-    <v-card>
-      <v-subheader>SELECT PACKAGE(S)</v-subheader>
-      <cv-multi-select
-        :label="pkgSelectLabel"
-        :options="items"
-        v-model="selectedPkg"
-      >
-      </cv-multi-select>
-    </v-card>
-    <v-card>
-      <Network :data-promise="depData" :key="componentKey"></Network>
-    </v-card>
-    </v-col>
-
-    <v-col
-      cols="12"
-      sm="4"
-    >
-    <v-card>
-      <v-subheader>SEARCH FOR PACKAGE</v-subheader>
-      <cv-search
-        v-model="searchPkg">
-      </cv-search>
-      <cv-data-table
-        :columns="searchColumns"
-        :data="searchItems"
-        @sort="onSort"
-      >
-        <template slot="headings">
-          <cv-data-table-heading heading="Channel" sortable />
-          <cv-data-table-heading heading="Build" sortable />
-        </template>
-      </cv-data-table>
-    </v-card>
-    </v-col>
-    </v-row>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <Header></Header>
+    <cv-content id="main-content" class="container">
+      <router-view />
+    </cv-content>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import {
-  CvDataTable,
-  CvDataTableHeading,
-  CvMultiSelect,
-  CvSearch
-} from '@carbon/vue';
-import Network from './components/Network';
-
+import Header from './components/Header';
 export default {
   name: 'App',
-
   components: {
-    CvDataTable,
-    CvDataTableHeading,
-    CvMultiSelect,
-    CvSearch,
-    Network
-  },
-
-  data: () => ({
-    envs: [],
-    envSelectLabel: 'Click one environment prefix',
-    selectedEnv: [],
-    items: [],
-    pkgSelectLabel: 'View dependency graph for...',
-    selectedPkg: [],
-    searchPkg: '',
-    searchItems: [],
-    baseUrl: 'http://0.0.0.0:5000',
-    depUrl: [],
-    depData: [],
-    componentKey: 0,
-    columns: [{text: "Package", value: "name"},
-              {text: "Version", value: "version", sortable: false}],
-    searchColumns: ["Channel", "Build"]
-  }),
-  watch: {
-    // whenever selectedEnv changes, this function will run
-    selectedEnv: function () {
-      this.getEnvName(),
-      this.getPkgs()
-    },
-    // whenever selectedPkg changes, this function will run
-    selectedPkg: function () {
-      this.getDepGraph()
-    },
-    // whenever searchPkg changes, this function will run
-    searchPkg: function () {
-      this.getChannels()
-    },
-  },
-
-  mounted: function() {
-    this.getEnvs();
-  },
-  methods: {
-    onSort(sortBy) {
-      if (sortBy) {
-        this.searchItems.sort((a, b) => {
-          const itemA = a[sortBy.index];
-          const itemB = b[sortBy.index];
-          if (sortBy.order === 'descending') {
-            if (sortBy.index === 2) {
-              // sort as number
-              return parseFloat(itemA) - parseFloat(itemB);
-            } else {
-              return itemB.localeCompare(itemA);
-            }
-          }
-          if (sortBy.order === 'ascending') {
-            if (sortBy.index === 2) {
-              // sort as number
-              return parseFloat(itemB) - parseFloat(itemA);
-            } else {
-              return itemA.localeCompare(itemB);
-            }
-          }
-          return 0;
-        });
-      }
-    },
-    getEnvs() {
-      let url = this.baseUrl + '/' + 'envs';
-      axios.get(url).then((response) => {
-        let li = response.data.envs;
-        this.envs = li.map(el => {
-          let envName = el.split('/').pop();
-          if (envName.includes('miniconda') || envName.includes('anaconda')) {
-            envName = 'base';
-          }
-          return {
-            name: el,
-            label: el,
-            value: envName,
-          };
-        })
-      }).catch(error => { console.log(error); });
-    },
-    getEnvName() {
-      // forces single select
-      this.selectedEnv = this.selectedEnv.pop();
-    },
-    getPkgs() {
-      let reqUrl = this.baseUrl + '/envs/' + this.selectedEnv;
-      axios.get(reqUrl).then((response) => {
-        let pk = response.data;
-        this.items = pk.map(el => {
-          return {
-            name: el.name,
-            label: el.name + ' ' + el.version,
-            value: el.name,
-          };
-        })
-      }).catch(error => { console.log(error); });
-    },
-    getDepGraph() {
-      this.componentKey += 1;
-      let url = this.baseUrl + '/pkgs';
-      this.depUrl = this.selectedPkg.map(function (i) { return url + '/' + i });
-      this.depData = this.depUrl.map(u => fetch(u).then(resp => resp.json()));
-    },
-    getChannels() {
-      let reqUrl = this.baseUrl + '/search/' + this.searchPkg;
-      axios.get(reqUrl).then((response) => {
-        let pk = response.data.result.pkgs;
-        this.searchItems = pk.map(function(el) {
-          return [el.channel, el.build];
-        })
-      }).catch(error => { console.log(error); });
-    },
-  },
+    Header
+  }
 };
 </script>
 
 <style lang="scss">
-@import './styles/carbon';
+  @import './styles/carbon';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
