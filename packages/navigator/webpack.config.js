8b0063186d3364ba6e4284daede33470a3fdfebe
Frederic Collonval <fcollonval@gmail.com>
2020-11-11 14:13:31 +0100

    Correct public path to match app url

--- packages/navigator/webpack.config.js
+++ packages/navigator/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = [
         'static'
       ),
       filename: 'navigator.bundle.js',
-      publicPath: './static/'
+      publicPath: './static/gator/'
     },
     bail: true,
     devtool: 'source-map',
