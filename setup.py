4a255878e125ee11d16da45c81a977331db35f1f
Frédéric Collonval <fcollonval@gmail.com>
2021-09-03 09:25:17 +0200

    Bump to 5.1.2 (#154)
    * Bump to 5.1.2
    
    * Bump to 3.0.2
    
    * Bump to 3.0.2
    
    * Bump to 3.0.2
    
    * Bump to 3.0.2
    
    * Add keyword and classifiers
    
    * Update README.md

--- setup.py
+++ setup.py
@@ -96,4 +96,19 @@ setuptools.setup(
         ]
     },
     entry_points={"console_scripts": ["gator = mamba_gator.navigator.main:main"]},
+    keywords=["Jupyter", "JupyterLab", "JupyterLab3", "conda", "mamba"],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: Jupyter",
+        "Framework :: Jupyter :: JupyterLab",
+        "Framework :: Jupyter :: JupyterLab :: 3",
+        "Framework :: Jupyter :: JupyterLab :: Extensions",
+        "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
+    ],
 )
