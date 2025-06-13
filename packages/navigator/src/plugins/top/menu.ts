996e72c6596687639306a2347081bac51c8fd836
Dan Yeaw <dan@yeaw.me>
2025-06-13 15:39:41 -0400

    JL4: Add compatibility for Menus with RankedMenu (#247)

--- packages/navigator/src/plugins/top/menu.ts
+++ packages/navigator/src/plugins/top/menu.ts
@@ -1,12 +1,14 @@
-import { IJupyterLabMenu } from '@jupyterlab/mainmenu';
-
 import { CommandRegistry } from '@lumino/commands';
-
-import { MenuBar } from '@lumino/widgets';
-
+import { MenuBar, Menu } from '@lumino/widgets';
 import { HelpMenu } from './help';
-
 import { IMainMenu } from './tokens';
+import { isJupyterLab4, getMenuInterface } from './utils';
+
+// TODO: Replace with this import when dropping JupyterLab 3 support:
+// import { IRankedMenu } from '@jupyterlab/ui-components';
+
+// Temporary compatibility for JupyterLab 3 & 4 interface types
+type IMenuInterface = ReturnType<typeof getMenuInterface>;
 
 /**
  * The main menu.
@@ -22,13 +24,20 @@ export class MainMenu extends MenuBar implements IMainMenu {
     const { commands } = options;
     this._helpMenu = new HelpMenu({ commands });
 
-    this.addMenu(this._helpMenu.menu);
+    // Handle the API difference between JupyterLab 3 and 4:
+    // - JL3: need to access .menu property
+    // - JL4: HelpMenu extends Menu directly
+    const menuToAdd: Menu = isJupyterLab4()
+      ? (this._helpMenu as any as Menu) // Cast to Menu since it extends Menu in JL4
+      : (this._helpMenu as any).menu; // Access .menu property in JL3
+
+    this.addMenu(menuToAdd);
   }
 
   /**
    * Get the help menu.
    */
-  get helpMenu(): IJupyterLabMenu {
+  get helpMenu(): IMenuInterface {
     return this._helpMenu;
   }
 
@@ -36,7 +45,7 @@ export class MainMenu extends MenuBar implements IMainMenu {
 }
 
 /**
- * A namespaces for `MainMenu` statics.
+ * A namespace for `MainMenu` statics.
  */
 export namespace MainMenu {
   /**
