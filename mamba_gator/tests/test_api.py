5359ea3f374877e346d9f9c1fbcb136b3f8c1087
RRosio <rosio_reyes@yahoo.com>
2025-10-21 13:48:44 -0700

    remove python constraint

--- mamba_gator/tests/test_api.py
+++ mamba_gator/tests/test_api.py
@@ -54,7 +54,7 @@ class JupyterCondaAPITest(ServerTest):
 
         return self.conda_api.post(
             ["environments"],
-            body={"name": new_name, "packages": packages or ["python!=3.14.0"]},
+            body={"name": new_name, "packages": packages or ["python"]},
         )
 
     def rm_env(self, name):
