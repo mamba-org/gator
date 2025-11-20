13609961783255bccf266e67165ab1c5de1b9d39
RRosio <rosio_reyes@yahoo.com>
2025-11-19 21:45:53 -0800

    change to book-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,10 +18,12 @@ extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",      # module index
     "sphinx.ext.autosummary",
+    "sphinx_design"
 ]
 
 source_suffix = {
     '.md': 'markdown',
+    '.rst': 'restructuredtext'
 }
 
 myst_enable_extensions = [
@@ -30,6 +32,17 @@ myst_enable_extensions = [
     "linkify",
     "substitution"
 ]
+
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/mamba-org/gator",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_source_button": True,
+    "path_to_docs": "docs/source",
+    "show_toc_level": 2,
+    "announcement": "🚧 Documentation is under active development. It will be undergoing many changes and additions to the currently displayed material. <a href='https://github.com/mamba-org/gator/issues'>Report issues</a>",
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -43,5 +56,3 @@ sys.path.insert(0, str(Path("../../mamba_gator").resolve()))
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
